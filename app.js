7737b680a7a2911c7503a74664603504399af97d
Joshua Sung <yuniksung@gmail.com>
2020-11-16 19:55:31 -0600

    chapter 2

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-      console.log(profileDataArr[i]);
+const profileDataArgs= process.argv.slice(2, process.argv.length);
+//console.log(profileDataArgs);
+
+const printProfileData= (profileDataArr) => {
+    for(let i=0; i<profileDataArr.length; i += 1){
+        console.log(profileDataArr[i]);
     }
 
-  
-  console.log('================');
+    console.log('================');
 
-//   profileDataArr.forEach((profileItem) => {
-//       console.log(profileItem);
-//   });
-profileDataArr.forEach(profileItem => console.log(profileItem));
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
 };
 
- 
+printProfileData(profileDataArgs);
