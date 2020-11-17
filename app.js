7bf2fdfb29316654ac6125cb14bfae39e8f51986
Joshua Sung <yuniksung@gmail.com>
2020-11-16 23:01:02 -0600

    middle of chapter 3

---
+++ app.js
@@ -0,0 +1,23 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+// console.log(inquirer);
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template');
+
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
