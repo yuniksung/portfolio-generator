aa1cf9aee515d2a3484c9ce9ac426a0b60e3122d
Joshua Sung <yuniksung@gmail.com>
2020-11-16 19:27:07 -0600

    1

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++) {
       console.log(profileDataArr[i]);
