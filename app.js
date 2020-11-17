a12639613b37762cbe6676a921dd23a3107e4f87
Joshua Sung <yuniksung@gmail.com>
2020-11-16 19:53:27 -0600

    chapter 2 not finished

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-      console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//       console.log(profileDataArr[i]);
+//     }
 
   
-  console.log('================');
+//   console.log('================');
 
 //   profileDataArr.forEach((profileItem) => {
 //       console.log(profileItem);
 //   });
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
- 
+//  const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+//  console.log(generatePage('jane', 'janehub'));
+const [name, github] = profileDataArgs;
+
+
+const generatePage = (userName, githubName) => {
+  
+  return `
+  Name: ${userName}
+  Github: ${githubName}
+  `;
+}
+console.log(generatePage(name, github));
